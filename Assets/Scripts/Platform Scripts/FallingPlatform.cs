c01620731cbcb40c06af55311ab1287590d1ddae
Treyvion Arte Johnson <tajohns22@gmail.com>
2023-03-16 21:58:30 -0500

    fixed jump and added falling platform

---
+++ Assets/Scripts/Platform Scripts/FallingPlatform.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+
+    private Collider2D _collider2D;
+    private float _destroyDelay = 1.4f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField, Range(0, 1f)]
+    [Tooltip("The amount of delay we want before the plat form falls")]
+    public float fallDelay;
+
+    private void Awake()
+    {
+        _collider2D = GetComponent<Collider2D>();
+    }
+
+    private IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(gameObject, _destroyDelay);
+    }
+    
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
