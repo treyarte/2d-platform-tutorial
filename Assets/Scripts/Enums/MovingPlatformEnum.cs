d09cf443ff8474fdc4efcc667e75cd22f5822418
Treyvion Arte Johnson <tajohns22@gmail.com>
2023-04-02 14:41:51 -0500

    fixed moving platform slow player movement issue

---
+++ Assets/Scripts/Enums/MovingPlatformEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Enums
+{
+    public enum MovingPlatformEnum
+    {
+        UpAndDown = 0,
+        LeftAndRight = 1
+    }
+}
--- Assets/Scripts/Platform Scripts/MovingPlatform.cs
+++ Assets/Scripts/Platform Scripts/MovingPlatform.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Enums;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -12,8 +13,18 @@ namespace Platform_Scripts
         [SerializeField] private int StartingPoint;
         [SerializeField] private List<Transform> ListOfPoints;
         [SerializeField] private float cutOffDistance = 0.95f;
+        [SerializeField] private MovingPlatformEnum type; 
 
         private int i;
+
+        public void Awake()
+        {
+            if (type == MovingPlatformEnum.UpAndDown)
+            {
+                transform.position =
+                    new Vector3(ListOfPoints[0].position.x, transform.position.y, transform.position.z);
+            }
+        }
 
         public void Start()
         {
@@ -32,13 +43,21 @@ namespace Platform_Scripts
                 }
             }
 
-            transform.position = Vector2.MoveTowards(transform.position, ListOfPoints[i].position,
+            var pointPosition = ListOfPoints[i].position;
+
+            transform.position = Vector2.MoveTowards(transform.position, pointPosition,
                 Speed * Time.deltaTime);
         }
 
+        /// <summary>
+        /// When we a player collide with the moving platform
+        /// we set the platform (what is rendered) as the player parent.
+        /// </summary>
+        /// <param name="col"></param>
         private void OnCollisionEnter2D(Collision2D col)
         {
-            if (col.gameObject.CompareTag("Player"))
+            //Check if what collide has the playertag
+             if (col.gameObject.CompareTag("Player"))
             {
                 if (CheckIfPlayerIsOnPlatform(col.gameObject))
                 {
@@ -46,24 +65,35 @@ namespace Platform_Scripts
                     if (playerBody)
                     {
                         col.transform.SetParent(transform);
-                        // playerBody.velocity =
-                        //     Vector2.MoveTowards(col.gameObject.transform.position, transform.position, Speed);
+                        // To stop the jitteriness we have tp set the player's rb interpolation to none
+                        col.rigidbody.interpolation = RigidbodyInterpolation2D.None;
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// When the player leaves the platform set the player back
+        /// to how it was before
+        /// </summary>
+        /// <param name="col"></param>
         private void OnCollisionExit2D(Collision2D col)
         {
-            if (col.gameObject.CompareTag("Player"))
-            {
-                col.transform.SetParent(null);
-            }
+            if (!col.gameObject.CompareTag("Player")) return;
+            
+            col.transform.SetParent(null);
+            col.rigidbody.interpolation = RigidbodyInterpolation2D.Interpolate;
         }
 
+        /// <summary>
+        /// Check if the player is standing on the platform
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
         private bool CheckIfPlayerIsOnPlatform(GameObject player)
         {
 
+            //This is for low to the ground platforms
             // if (transform.position.y > player.transform.position.y )
             // {
             //     return false;
@@ -76,7 +106,7 @@ namespace Platform_Scripts
                 return false;
             }
             
-            return playerBody.velocity.y == 0;
+            return playerBody.velocity.y <= 0;
 
         }
     }
